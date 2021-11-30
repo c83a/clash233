0496fc8e08215c043119fde607aae226e67663e8
jw853355718 <54950939+jw853355718@users.noreply.github.com>
2021-11-30 15:42:14 +0800

    Update post_wan_script.sh

--- post_wan_script.sh
+++ post_wan_script.sh
@@ -9,11 +9,15 @@ set -x
 logger  "运行后 WAN 状态:" "WAN 状态:【$1】, WAN 接口:【$2】, WAN IP:【$3】"
 link=$1
 wan_if=$2
-
+change_route6(){
+src="$1"
+dst="$2"
+[ "0" == "0$src" ] || [ "0" == "0$dst" ] && return
+ip -6 route list  | grep $src | awk '!a[$0]++' | sed -E "s/$src/$dst/g;s/expires .*//g" | awk '{print "ip -6 route change",$0}' | sh
+}
 bridge_ipv6(){
 if [ "0$link" == "0down" ] && [ "0" != "0$wan_if" ]; then
-  ipv6_gw=`ip -6 route | grep "default via" | head -n 1 | awk '{print $3}'`
-  [ 0 == $? ] && logger "change $ipv6_gw $wan_if" && ip -6 route change default via $ipv6_gw dev $wan_if
+  logger "change route6 from br0 to $wan_if" && change_route6 "br0" "$wan_if"
   ipv6=`ifconfig br0 | grep inet6 | grep Global | awk '{print $3}'`
   [ 0 == $? ] && logger "del ipv6 address $ipv6 on br0" && ifconfig br0 del $ipv6
   logger "del $wan_if from bridge br0" && brctl delif br0 $wan_if
@@ -31,11 +35,10 @@ while true;do
   else
     logger "WAN IPv6: $ipv6"
     ifconfig br0 add $ipv6
-    ipv6_gw=`ip -6 route | grep "default via" | head -n 1 | awk '{print $3}'`
     ebtables -t broute --list | grep "\-p ! IPv6 -i $wan_if -j DROP"
     [ 0 == $? ] || ebtables -t broute -A BROUTING -p ! IPv6 -i $wan_if -j DROP
     brctl addif br0 $wan_if
-    ip -6 route change default via $ipv6_gw dev br0
+    change_route6 $wan_if br0
     break
   fi
 done
