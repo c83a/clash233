d24649e0f925366757bcf234b62652c1dc7eac74
jw853355718 <17853355718@139.com>
2023-01-08 12:40:54 +0800

    update

--- add_ip.py
+++ add_ip.py
@@ -13,49 +13,60 @@ def get_location(ip):
   return response['country']['iso_code']
 def get_file():
   try:
-    open(sys.argv[1]) as f:
+    f=open(sys.argv[1])
   except:
     f=sys.stdin
   yield from f
-async def a_read(gen):
+  raise EOFError()
+async def a_read():
   loop=asyncio.get_running_loop()
-  with concurrent.futures.ThreadPoolExecutor(max_worker=1) as pool:
-    try:
-      r = await loop.run_in_executor(pool, lambda: next(gen))
-      yield r
-    except StopIteration:
-      pass
-async def print_item(agen):
+  gen=get_file()
+  with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:
+    while True:
+      try:
+        r = await loop.run_in_executor(pool, lambda: next(gen))
+        yield r
+      except EOFError:
+            break
+async def print_item(agen,alock):
   nslookup46=asyncio.get_running_loop().getaddrinfo
-  async for line in agen:
-    await asyncio.sleep(0)
-    for server in re.finditer('server: ([^,]*)',line):
-      ip_domain=server.group(1)
-      if re.match(ip_pattern,ip_domain):
-        ip=ip_domain
-      elif ip_domain in dns_cache:
-        ip=dns_cache[ip_domain]
-      else:
-        domain=ip_domain
-        ip=None
-        try:
-          ip=(await nslookup46(domain,80))[0][4][0]
-        except:
-          pass
-        dns_cache[domain]=ip
-      if ip in code_cache:
-          code=code_cache[ip]
-      else:
-        try:
-          code=get_location(ip)
-        except:
-          code='ZZ'
-        code_cache[ip]=code
-      print("#".join(map(str,(line.strip(), code, ip))))
-      break
+  while True:
+    line=None
+    async with alock:
+        async for line in agen:
+            break
+    if line is None:
+        break
+    else:
+      await asyncio.sleep(0)
+      for server in re.finditer('server: ([^,]*)',line):
+        ip_domain=server.group(1)
+        if re.match(ip_pattern,ip_domain):
+          ip=ip_domain
+        elif ip_domain in dns_cache:
+          ip=dns_cache[ip_domain]
+        else:
+          domain=ip_domain
+          ip=None
+          try:
+            ip=(await nslookup46(domain,80))[0][4][0]
+          except:
+            pass
+          dns_cache[domain]=ip
+        if ip in code_cache:
+            code=code_cache[ip]
+        else:
+          try:
+            code=get_location(ip)
+          except:
+            code='ZZ'
+          code_cache[ip]=code
+        print("#".join(map(str,(line.strip(), code, ip))))
+        break
 async def main():
-  agen=a_read(get_file())
-  task_list=[asyncio.create_task(print_item(agen) for i in range(20)]
+  alock=asyncio.Lock()
+  agen=a_read()
+  task_list=[asyncio.create_task(print_item(agen,alock)) for i in range(20)]
   print('proxies:')
   await asyncio.gather(*task_list,return_exceptions=True)
 asyncio.run(main())
