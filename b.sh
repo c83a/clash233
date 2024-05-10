a84d5e4504e43dd947d7bb2f6c457e70a1632b98
c83a <141380034+c83a@users.noreply.github.com>
2024-05-10 17:48:03 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,21 @@
 mkdir ~/.ssh
 echo "$SSH_PUBLIC_KEY" >> ~/.ssh/authorized_keys
-netstat -pna
+
+f(){
+sleep 10
+HAS_ERRORS=$(grep "command failed" < .ngrok.log)
+
+if [[ -z "$HAS_ERRORS" ]]; then
+  echo ""
+  echo "=========================================="
+  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+  echo "=========================================="
+else
+  echo "$HAS_ERRORS"
+  exit 4
+fi
+}
+
 if [[ -z "$NGROK_TOKEN" ]]; then
   echo "Please set 'NGROK_TOKEN'"
   exit 2
@@ -16,22 +31,7 @@ echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
 
 echo "### Start ngrok proxy for 22 port ###"
 
-
+f &
 #rm -f .ngrok.log
 ngrok authtoken "$NGROK_TOKEN"
-timeout 1h ngrok tcp 22 --log ".ngrok.log"  &
-
-
-sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
-
-if [[ -z "$HAS_ERRORS" ]]; then
-  echo ""
-  echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo "=========================================="
-else
-  echo "$HAS_ERRORS"
-  exit 4
-fi
-sleep 1h
+timeout 1h ngrok tcp 22 --log ".ngrok.log" 
